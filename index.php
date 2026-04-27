1d1eaf4fe119b0c4afdd3415d2033bcb3f9886d8
Oscar Victorious Putra Tambunan <oss.tam1137@gmail.com>
2026-04-27 18:08:30 +0700

    Add admin login and CRUD dashboard for products

--- index.php
+++ index.php
@@ -164,6 +164,7 @@ try {
         <nav>
             <a href="#berita">Berita</a>
             <a href="#produk">Produk</a>
+            <a href="/admin">Admin</a>
         </nav>
     </div>
 </header>
